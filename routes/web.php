f9126473c2da3dfa313a0d67e805c7f95b96a1b7
Christopher Carlos <cscarlos@iskolarngbayan.pup.edu.ph>
2022-07-05 09:01:23 +0800

    super admin system links

--- routes/web.php
+++ routes/web.php
@@ -234,13 +234,13 @@ Route::group(['middleware' => [
         Route::resource('AR-Events', 'App\Http\Controllers\AccomplishEventsCRUD');
         // Route::resource('articles', ArticleCreate::class);
 
-        Route::resource('sadmin-sois-sub-links', 'App\Http\Controllers\SoisSystemLinks');
-        Route::resource('sadmin-system-assets-type', 'App\Http\Controllers\SystemAssetTypes');
-        Route::resource('sadmin-web-page-type', 'App\Http\Controllers\WebPageType');
-        Route::resource('sadmin-officers', 'App\Http\Controllers\OfficerControl');
-        Route::resource('sadmin-position-titles', 'App\Http\Controllers\PositionTitles');
-        Route::resource('sadmin-organization', 'App\Http\Controllers\OrganizationCRUD');
-        Route::resource('admin-sliders', 'App\Http\Controllers\AdminSlider');
+        Route::resource('sadmin-sois-sub-links', 'App\Http\Controllers\SASoisSystemLinks');
+        Route::resource('sadmin-system-assets-type', 'App\Http\Controllers\SASystemAssetTypes');
+        Route::resource('sadmin-web-page-type', 'App\Http\Controllers\SAWebPageType');
+        Route::resource('sadmin-officers', 'App\Http\Controllers\SAOfficerControl');
+        Route::resource('sadmin-position-titles', 'App\Http\Controllers\SAPositionTitles');
+        Route::resource('sadmin-organization', 'App\Http\Controllers\SAOrganizationCRUD');
+        Route::resource('sadmin-sliders', 'App\Http\Controllers\SAAdminSlider');
 
         Route::put('users/addRoleToUser/{id}','App\Http\Controllers\UserCRUD@addRole')->name('users/addRoleToUser');
         Route::put('users/addOrganizationToUser/{id}','App\Http\Controllers\UserCRUD@addOrg')->name('users/addOrganizationToUser');
