77c1e9bf3a8623ecd755ebfd32eafd2ede892dd0
Christopher Carlos <cscarlos@iskolarngbayan.pup.edu.ph>
2022-07-06 12:26:22 +0800

    head system links

---
+++ resources/views/normLaravel/admin-system-links-edit.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.headlines')
+
+@section('page-title','SOIS | Edit System Links')
+
+@livewire('admin-nav-bars')
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+
+    <style>
+        .modal-backdrop {
+          z-index: -1;
+        }
+    </style>
+
+<div class="py-12">
+	<div class="max-w-11xl mx-auto sm:px-6 lg:px-8">
+	    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+			<div class="grid grid-cols-12">
+			    <div class="col-span-12">
+			        <h2>Update News</h2>
+			    </div>
+            </div>
+
+<div class="flex flex-wrap flex-row">
+	@if(session('status'))
+	  <div class="alert alert-success">
+	      {{ session('status') }}
+	  </div>
+	@endif	
+</div>
+
+
+	<div class=" col-span-6 max-w-lg rounded overflow-hidden shadow-lg">
+		<form name="add-articles" id="add-articles" method="post" action="{{ route('admin-sois-sub-links.update', $slink->sois_links_id ) }}" enctype="multipart/form-data">
+		@csrf
+		@method('PUT')
+			<div class="px-6 py-4">
+				@foreach($selectedLink as $selectLinks)
+				<div class="form-group">
+					<label for="link_name">Link Name :</label>
+					<input type="text" id="link_name" name="link_name" placeholder= "{{$selectLinks->link_name}}" class="form-control" >
+				</div>
+				<div class="form-group">
+					<label for="link_description">Link Description : </label>
+					<input type="text" id="link_description" name="link_description" placeholder="{{$selectLinks->link_description}}" class="form-control" >
+				</div>
+				<div class="form-group">
+					<label for="external_link">Link Description :</label>
+					<input type="text" id="external_link" name="external_link" placeholder="{{$selectLinks->external_link}}" class="form-control" >
+				</div>
+				@endforeach
+	  		</div>
+	  		<div class="px-6 pt-4 pb-2">
+				<button class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" type="submit" class="btn btn-primary">Submit</button>
+	  		</div>
+		</form>
+	</div>
+</div>
+
+
+
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+@extends('layouts.closing-tag')
